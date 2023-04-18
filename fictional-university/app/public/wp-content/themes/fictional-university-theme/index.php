0edbee9937d2f2d8f553e075894d6854113d1e65
Thanh Nguyen <joseph_nguyen1094@yahoo.com>
2023-04-18 17:59:35 +0700

    Blog2

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/index.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/index.php
@@ -26,6 +26,7 @@
 
       <div class="generic-content">
         <?php the_excerpt(); ?>
+        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
       </div>
 
     </div>
