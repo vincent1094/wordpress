fb2a644881a64aeef88f06e18dc973e1216ca294
thanh <thanhn1094@gmail.com>
2023-04-22 14:30:09 +0700

    slug change

--- fictional-university/app/public/wp-content/mu-plugins/university_post_types.php
+++ fictional-university/app/public/wp-content/mu-plugins/university_post_types.php
@@ -1,6 +1,10 @@
 <?php 
     function university_post_types() {
         register_post_type('event', array(
+            'rewrite' => array(
+                'slug' => 'events'
+            ),
+            'has_archive' => true,
             'public' => true,
             'show_in_rest' => true,
             'labels' => array(
