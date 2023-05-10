98b7790745a99e3835af0df6a2266d8731c3ad5c
Thanh Nguyen <joseph_nguyen1094@yahoo.com>
2023-05-10 16:36:45 +0700

    Manipulating Default URL Based Queries

--- fictional-university/app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -19,9 +19,21 @@
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
             <?php 
+                $today = date('Ymd');
                 $homePageEvents = new WP_Query(array(
                     'posts_per_page' => 2,
-                    'post_type' => 'event'
+                    'post_type' => 'event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'numeric'
+                        )
+                    )
                 ));
 
                 while($homePageEvents->have_posts()) {
@@ -29,8 +41,11 @@
             ?>
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month">Mar</span>
-                        <span class="event-summary__day">25</span>
+                        <span class="event-summary__month"><?php 
+                        $eventDate = new DateTime(get_field('event_date'));
+                        echo $eventDate->format('M');
+                        ?></span>
+                        <span class="event-summary__day"><?php echo $eventDate->format('d');?></span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
